121e9c57ebb3a3c7b4ac1cee09c78db2c0c0edaa
Kaiyuan Wang <wangkaiyuanzz@gmail.com>
2017-11-15 23:24:37 -0600

    Update models.sh

--- models.sh
+++ models.sh
@@ -9,83 +9,19 @@ TEST_GEN_DIR="${GEN_DIR}/tests"
 TEST_DIR="${EXPERIMENT_DIR}/test-suite"
 
 MODELS=(
-        # "addr"
-        # "bempl"
-        # "binaryTree"
-        # "ctree"
-        # "dijkstra"
-        "dtree"
-        # "farmer"
-        # "fullTree"
-        # "grade"
-        # "grand"
-        # "handshake"
-        # "nqueens"
-        # "other"
-        # "singlyLinkedList"
-)
-
-declare -g -A singlyLinkedList=(
-        [model_name]="singlyLinkedList"
-        [model_path]="${MODEL_DIR}/singlyLinkedList.als"
-        [mutant_dir_path]="${GEN_DIR}/singlyLinkedList"
-        [scope]="3"
-        [test_gen_path]="${TEST_GEN_DIR}/singlyLinkedList.als"
-        [test_path]="${TEST_DIR}/singlyLinkedList.als"
-)
-
-declare -g -A binaryTree=(
-        [model_name]="binaryTree"
-        [model_path]="${MODEL_DIR}/binaryTree.als"
-        [mutant_dir_path]="${GEN_DIR}/binaryTree"
-        [scope]="3"
-        [test_gen_path]="${TEST_GEN_DIR}/binaryTree.als"
-        [test_path]="${TEST_DIR}/binaryTree.als"
-)
-
-declare -g -A fullTree=(
-        [model_name]="fullTree"
-        [model_path]="${MODEL_DIR}/fullTree.als"
-        [mutant_dir_path]="${GEN_DIR}/fullTree"
-        [scope]="3"
-        [test_gen_path]="${TEST_GEN_DIR}/fullTree.als"
-        [test_path]="${TEST_DIR}/fullTree.als"
-)
-
-declare -g -A handshake=(
-        [model_name]="handshake"
-        [model_path]="${MODEL_DIR}/handshake.als"
-        [mutant_dir_path]="${GEN_DIR}/handshake"
-        [scope]="5"
-        [test_gen_path]="${TEST_GEN_DIR}/handshake.als"
-        [test_path]="${TEST_DIR}/handshake.als"
-)
-
-declare -g -A nqueens=(
-        [model_name]="nqueens"
-        [model_path]="${MODEL_DIR}/nqueens.als"
-        [mutant_dir_path]="${GEN_DIR}/nqueens"
-        [scope]="4"
-        [test_gen_path]="${TEST_GEN_DIR}/nqueens.als"
-        [test_path]="${TEST_DIR}/nqueens.als"
-)
-
-declare -g -A farmer=(
-        [model_name]="farmer"
-        [model_path]="${MODEL_DIR}/farmer.als"
-        [mutant_dir_path]="${GEN_DIR}/farmer"
-        [scope]="4"
-        [test_gen_path]="${TEST_GEN_DIR}/farmer.als"
-        [test_path]="${TEST_DIR}/farmer.als"
-)
-
-declare -g -A dijkstra=(
-        [model_name]="dijkstra"
-        [model_path]="${MODEL_DIR}/dijkstra.als"
-        [mutant_dir_path]="${GEN_DIR}/dijkstra"
-        [scope]="3"
-        [test_gen_path]="${TEST_GEN_DIR}/dijkstra.als"
-        [test_path]="${TEST_DIR}/dijkstra.als"
+        "addr"
+        "bempl"
+        "binaryTree"
+        "ctree"
+        "dijkstra"
+        "farmer"
+        "fullTree"
+        "grade"
+        "grand"
+        "handshake"
+        "nqueens"
+        "other"
+        "singlyLinkedList"
 )
 
 declare -g -A addr=(
@@ -106,6 +42,15 @@ declare -g -A bempl=(
         [test_path]="${TEST_DIR}/bempl.als"
 )
 
+declare -g -A binaryTree=(
+        [model_name]="binaryTree"
+        [model_path]="${MODEL_DIR}/binaryTree.als"
+        [mutant_dir_path]="${GEN_DIR}/binaryTree"
+        [scope]="3"
+        [test_gen_path]="${TEST_GEN_DIR}/binaryTree.als"
+        [test_path]="${TEST_DIR}/binaryTree.als"
+)
+
 declare -g -A ctree=(
         [model_name]="ctree"
         [model_path]="${MODEL_DIR}/ctree.als"
@@ -115,13 +60,31 @@ declare -g -A ctree=(
         [test_path]="${TEST_DIR}/ctree.als"
 )
 
-declare -g -A dtree=(
-        [model_name]="dtree"
-        [model_path]="${MODEL_DIR}/dtree.als"
-        [mutant_dir_path]="${GEN_DIR}/dtree"
+declare -g -A dijkstra=(
+        [model_name]="dijkstra"
+        [model_path]="${MODEL_DIR}/dijkstra.als"
+        [mutant_dir_path]="${GEN_DIR}/dijkstra"
+        [scope]="3"
+        [test_gen_path]="${TEST_GEN_DIR}/dijkstra.als"
+        [test_path]="${TEST_DIR}/dijkstra.als"
+)
+
+declare -g -A farmer=(
+        [model_name]="farmer"
+        [model_path]="${MODEL_DIR}/farmer.als"
+        [mutant_dir_path]="${GEN_DIR}/farmer"
         [scope]="4"
-        [test_gen_path]="${TEST_GEN_DIR}/dtree.als"
-        [test_path]="${TEST_DIR}/dtree.als"
+        [test_gen_path]="${TEST_GEN_DIR}/farmer.als"
+        [test_path]="${TEST_DIR}/farmer.als"
+)
+
+declare -g -A fullTree=(
+        [model_name]="fullTree"
+        [model_path]="${MODEL_DIR}/fullTree.als"
+        [mutant_dir_path]="${GEN_DIR}/fullTree"
+        [scope]="3"
+        [test_gen_path]="${TEST_GEN_DIR}/fullTree.als"
+        [test_path]="${TEST_DIR}/fullTree.als"
 )
 
 declare -g -A grade=(
@@ -142,6 +105,24 @@ declare -g -A grand=(
         [test_path]="${TEST_DIR}/grand.als"
 )
 
+declare -g -A handshake=(
+        [model_name]="handshake"
+        [model_path]="${MODEL_DIR}/handshake.als"
+        [mutant_dir_path]="${GEN_DIR}/handshake"
+        [scope]="5"
+        [test_gen_path]="${TEST_GEN_DIR}/handshake.als"
+        [test_path]="${TEST_DIR}/handshake.als"
+)
+
+declare -g -A nqueens=(
+        [model_name]="nqueens"
+        [model_path]="${MODEL_DIR}/nqueens.als"
+        [mutant_dir_path]="${GEN_DIR}/nqueens"
+        [scope]="4"
+        [test_gen_path]="${TEST_GEN_DIR}/nqueens.als"
+        [test_path]="${TEST_DIR}/nqueens.als"
+)
+
 declare -g -A other=(
         [model_name]="other"
         [model_path]="${MODEL_DIR}/other.als"
@@ -149,4 +130,13 @@ declare -g -A other=(
         [scope]="3"
         [test_gen_path]="${TEST_GEN_DIR}/other.als"
         [test_path]="${TEST_DIR}/other.als"
+)
+
+declare -g -A singlyLinkedList=(
+        [model_name]="singlyLinkedList"
+        [model_path]="${MODEL_DIR}/singlyLinkedList.als"
+        [mutant_dir_path]="${GEN_DIR}/singlyLinkedList"
+        [scope]="3"
+        [test_gen_path]="${TEST_GEN_DIR}/singlyLinkedList.als"
+        [test_path]="${TEST_DIR}/singlyLinkedList.als"
 )
